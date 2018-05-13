1f8230659c5454ede104c82f31304f0a122fb2ce
hbj <huangbanji.com>
2018-05-13 22:06:06 +0800

    mv ip

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,5 @@
 application = ENV['app'] || "etm-frontend"
-server = ENV['server_app'] || "root@192.168.1.106"
+server = ENV['server_app'] || "root@192.168.1.102"
 set :application, application
 set :stage, :staging
 set :branch, :staging
